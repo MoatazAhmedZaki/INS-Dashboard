6d6ff30884e77591e421fbc4fb9e4cb6426843ad
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 12:20:32 +0200

    Updates

---
+++ precache-manifest.ee9c6c2a8790e9a302930b07aedbea5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3be1b184103740cdd7565defd1a4995",
+    "url": "/IUS-Dashboard/index.html"
+  },
+  {
+    "revision": "2208f75f10fc93a718ea",
+    "url": "/IUS-Dashboard/static/css/2.4dcd24aa.chunk.css"
+  },
+  {
+    "revision": "17cfa52aad37da69397b",
+    "url": "/IUS-Dashboard/static/css/main.8e54c60a.chunk.css"
+  },
+  {
+    "revision": "2208f75f10fc93a718ea",
+    "url": "/IUS-Dashboard/static/js/2.18004952.chunk.js"
+  },
+  {
+    "revision": "17cfa52aad37da69397b",
+    "url": "/IUS-Dashboard/static/js/main.0275c481.chunk.js"
+  },
+  {
+    "revision": "66045a8e3fa8fe17b68c",
+    "url": "/IUS-Dashboard/static/js/runtime-main.3d8b789c.js"
+  },
+  {
+    "revision": "f496cc041e4c27341cdc8de692235ddf",
+    "url": "/IUS-Dashboard/static/media/BG1.f496cc04.png"
+  },
+  {
+    "revision": "17efa687d8746dbe51c6046199c62ff3",
+    "url": "/IUS-Dashboard/static/media/calendar-regular.17efa687.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/INS-Dashboard/precache-manifest.275495df7aa451d097c92938901e17d8.js"
+  "/IUS-Dashboard/precache-manifest.ee9c6c2a8790e9a302930b07aedbea5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/INS-Dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IUS-Dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
